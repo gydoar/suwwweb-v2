12a3dc6900fcd7b2ef468f2bce76d05ae2588db0
Andres Vega <gydoar@hotmail.com>
2016-04-21 09:22:52 -0500

    agregando red de busqueda

--- red-busqueda.php
+++ red-busqueda.php
@@ -2,8 +2,8 @@
 
 <div class="header-3">
 	<div class="container center">
-		<h3>EL PLAN PARA LAS EMPRESAS</h3>
-		<h4>Creado para empresas destacadas en el mercado y que desean abrirse al mundo web.</h4>
+		<h3>RED DE BUSQUEDA</h3>
+		<h4>Esta diseñado a tus necesidades de emprendedor para que de esta manera comiences a dar a conocer tus productos o servicios en la web.</h4>
 	</div>
 </div>
 
@@ -13,28 +13,22 @@
 <!-- Descripcion general del plan -->
 
 <div class="container descrip__plan__general">
+	<div class="six columns offset-by-one center">
+		<img width="400px" src="assets/images/icons/google-adwords/red-busqueda.png" alt="">
+	</div>
 	<div class="four columns offset-by-one">
-		<h2><span>PLAN</span> <strong>EMPRESARIAL</strong></h2>
-
-		<div class="padding-left-2">
-			<p>Este plan esta diseñado a tus necesidades de emprendedor para que de esta manera comiences a dar a conocer tus.</p>
-		
-			<h3>PRECIO&nbsp;&nbsp;&nbsp; <span>$ 1.690.000</span></h3>
-		</div>
-		
 		<ul class="planes">
-            <li><p>Diseño unico</p></li>
-            <li><p>10 páginas internas</p></li>
-            <li><p>9 cuentas de correo</p></li>
+            <li><p>1 mes de optimización</p></li>
+            <li><p>Configuración y puesta en marcha de campaña.</p></li>
+            <li><p>Análisis de competencia</p></li>
         </ul>
+
+        <h3>PRECIO&nbsp;&nbsp;&nbsp; <span>$ 1.290.000</span></h3>
 		
         <div class="boton-principal center">
             <a class="button-green fancybox fancybox.iframe" href="contacto">ADQUIRIR</a>
         </div>
 
-	</div>
-	<div class="six columns offset-by-one center">
-		<img width="400px" src="assets/images/empresarial.png" alt="">
 	</div>
 </div>
 <div class="line__divisor"></div>
@@ -42,38 +36,58 @@
 <!-- Cacacteristicas todos los planes -->
  <div class="todos__los__planes center">
  	<div class="container">
-		<h3><strong>CARACTERISTICAS PLAN EMPRESARIAL</strong></h3>
+		<h3><strong>CARACTERISTICAS RED DE BUSQUEDA</strong></h3>
 		<h4>Seguridad, escalabilidad, disponibilidad, fiabilidad y rendimiento.</h4>
 		<div class="eight columns offset-by-two line__todos__nuestros__planes"><hr></div>
 		<!-- Fila 1 -->
 	 	<div class="row items__todos__planes">
 	 		<div class="four columns">
-	 			<img width="100px;" src="assets/images/todos-los-planes/icon-responsive.png" alt="">
-	 			<h5>DISEÑO UNICO Y RESPONSIVE</h5>
-	 			<p>No utilizamos plantillas, por eso todos nuestros diseños son diferentes y utilizamos las tendencias móviles del mercado.</p>
+	 			<img width="70px;" src="assets/images/icons/google-adwords/creacion-anuncios.png" alt="">
+	 			<h5>CREACIÓN DE ANUNCIOS</h5>
+	 			<p>Es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
 	 		</div>
 	 		<div class="four columns">
-	 			<img width="100px;" src="assets/images/todos-los-planes/icon-dominio.png" alt="">
-	 			<h5>DOMINIO (.COM .NET)</h5>
-	 			<p>Tu empresa necesita un nombre que lo represente en internet, por eso te entregamos de forma gratuita un dominio.</p>
+	 			<img width="70px;" src="assets/images/icons/google-adwords/configuracion-anuncios.png" alt="">
+	 			<h5>CONFIGURACIÓN DE ANUNCIOS</h5>
+	 			<p>Es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
 	 		</div>
 	 		<div class="four columns">
-	 			<img width="100px;" src="assets/images/todos-los-planes/icon-hosting.png" alt="">
-	 			<h5>ALOJAMIENTO (HOSTING)</h5>
-	 			<p>Su página web estará alojada en centros de datos seguros y que garantizan mostrar su sitio web en tiempos muy cortos.</p>
+	 			<img width="70px;" src="assets/images/icons/google-adwords/enlace-sitio.png" alt="">
+	 			<h5>ENLACE CON TU SITIO WEB</h5>
+	 			<p>Es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+	 		</div>
+	 	</div>
+
+	 	<div class="row items__todos__planes">
+	 		<div class="four columns">
+	 			<img width="70px;" src="assets/images/icons/google-adwords/google-analytics.png" alt="">
+	 			<h5>GOOGLE ANALYTICS</h5>
+	 			<p>Es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+	 		</div>
+	 		<div class="four columns">
+	 			<img width="70px;" src="assets/images/icons/google-adwords/analisis-palabras.png" alt="">
+	 			<h5>ANÁLISIS DE PALABRAS CLAVES</h5>
+	 			<p>Es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
+	 		</div>
+	 		<div class="four columns">
+	 			<img width="70px;" src="assets/images/icons/google-adwords/orientacion-geografica.png" alt="">
+	 			<h5>ORIENTACIÓN GEOGRÁFICA</h5>
+	 			<p>Es simplemente el texto de relleno de las imprentas y archivos de texto.</p>
 	 		</div>
 	 	</div>
 
 	 	<!-- Fila 3 -->
 	 	<div class="">
 	 		<ul class="items_small">
-	 			<li><p><img src="assets/images/icons/icon-check.png" alt=""> &nbsp;&nbsp;GOOGLE MY BUSINESS</p></li>
-	 			<li><p><img src="assets/images/icons/icon-check.png" alt=""> &nbsp;&nbsp;GOOGLE ANALYTICS</p></li>
-	 			<li><p><img src="assets/images/icons/icon-check.png" alt=""> &nbsp;&nbsp;GOOGLE MAPS</p></li>
+	 			<li><p><img src="assets/images/icons/icon-check.png" alt=""> &nbsp;&nbsp;OPTIMIZACIÓN DE PRESUPUESTO</p></li>
+	 			<li><p><img src="assets/images/icons/icon-check.png" alt=""> &nbsp;&nbsp;INCLUSIÓN DE NUEVOS ANUNCIOS</p></li>
 	 		</ul>
 	 	</div>
 	 </div>
  </div>
+
+
+
 
  <!-- Caracteristicas como empresa -->
  <div>
@@ -81,11 +95,11 @@
  </div>
  <!-- // Caracteristicas como empresa -->
 
- <div class="call-to-action-adquirir">
-	<div class="container center">
-		<p><span>PLAN EMPRESARIAL</span> IDEAL PARA PEQUEÑAS EMPRESAS  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a class="button-green-call fancybox fancybox.iframe" href="contacto">ADQUIRIR</a></p>
-	</div>
-</div>
+ <!-- Call to activon /Llamanos -->
+ <div>
+   <?php include "parts-body/call-to-action-adquirir.php"; ?>
+ </div>
+ <!-- // Call to activon /Llamanos -->
 
  <!-- Preguntas Frecuentes -->
  <div>
@@ -101,9 +115,9 @@
 
 		<div class="row">
 			<div class="four columns">
-				<a href="plan-personal.php">
-					<img width="100px;" src="assets/images/otros-productos/icon-personal.png" alt="">
-					<h2>PLAN PERSONAL</h2>
+				<a href="plan-empresarial">
+					<img width="100px;" src="assets/images/otros-productos/icon-empresarial.png" alt="">
+					<h2>PLAN EMPRESARIAL</h2>
 					<div class="eight columns offset-by-two line__otros__productos__2"><hr></div>
 
 					<p class="twelve columns">Su posicionamiento natural en buscadores es tan importante como la imagen de su empresa.</p>
