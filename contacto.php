6f6709c98756731112188cb61bf3de658b3b97c0
Andres Vega <gydoar@hotmail.com>
2016-05-14 12:51:19 -0500

    agrenado codigos enviados por Juan Carlos

--- contacto.php
+++ contacto.php
@@ -46,6 +46,29 @@
                 $_POST['telefono'] = '';
                 $_POST['cargo'] = '';
                 $_POST['mensaje'] = '';
+              ?>
+                <!-- Google Code for suWWWeb Conversion Page -->
+				<script type="text/javascript">
+				/* <![CDATA[ */
+				var google_conversion_id = 995508424;
+				var google_conversion_language = "en";
+				var google_conversion_format = "3";
+				var google_conversion_color = "ffffff";
+				var google_conversion_label = "Fad5CKCX7wMQyIHZ2gM";
+				var google_conversion_value = 1.00;
+				var google_conversion_currency = "COP";
+				var google_remarketing_only = false;
+				/* ]]> */
+				</script>
+				<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+				</script>
+				<noscript>
+				<div style="display:inline;">
+				<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/995508424/?value=1.00&amp;currency_code=COP&amp;label=Fad5CKCX7wMQyIHZ2gM&amp;guid=ON&amp;script=0"/>
+				</div>
+				</noscript>
+
+<?php
 
             }else{
                 $result = '<div class="result_fail">Hubo un error al enviar el mensaje </div>';
